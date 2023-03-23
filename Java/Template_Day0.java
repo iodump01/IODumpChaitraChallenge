9d398c6566253ae93144b01e2e6a03915465255c
Praneeth Shetty <64432063+ganimtron-10@users.noreply.github.com>
2023-03-23 22:46:09 +0530

    Create Template_Day0.java

---
+++ Java/Template_Day0.java
@@ -0,0 +1,13 @@
+// Template
+// Question
+
+// Program
+public class Template{
+  public static void main(String[] args){
+    System.out.println("IODumpChaitraChallenge");
+  }
+}
+
+// Input
+
+// Output
