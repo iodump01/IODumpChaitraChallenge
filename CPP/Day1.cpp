131da06dbbc5fb9d0abe92bd0dc80847d50edbea
Praneeth Shetty <praneethshetty10@gmail.com>
2023-03-25 22:57:10 +0530

    updating cpp code according to the template

--- CPP/Day1.cpp
+++ CPP/Day1.cpp
@@ -1,28 +1,50 @@
+/*
+
+Day 1: Program to find the largest and smallest of the given 5 numbers.
+
+*/
+
+// Program
+#include <iostream>
+using namespace std;
+
 int main()
 {
-    
+
     int array[5];
-    cout<<"enter 5 numbers : ";
-    for(int i = 0 ; i < 5 ; i++)
+    cout << "Enter 5 numbers space seperated: ";
+    for (int i = 0; i < 5; i++)
     {
-        cin>>array[i];
+        cin >> array[i];
     }
 
     int largest = array[0];
     int smallest = array[0];
 
-    for(int i = 0 ; i < 5 ; i++)
+    for (int i = 0; i < 5; i++)
     {
-        if(largest < array[i])
+        if (largest < array[i])
         {
             largest = array[i];
         }
-        if(smallest > array[i])
+        if (smallest > array[i])
         {
             smallest = array[i];
         }
     }
-    cout<<"largest number is : "<<largest<<endl;
-    cout<<"smallest number is : "<<smallest<<endl;
+    cout << "Largest number is : " << largest << endl;
+    cout << "Smallest number is : " << smallest << endl;
     return 0;
 }
+
+/*
+
+Input:
+Enter 5 numbers space seperated: 4 5 3 2 1
+
+Output:
+Largest number is : 5
+Smallest number is : 1
+
+
+*/
--- CPP/Day2.cpp
+++ CPP/Day2.cpp
@@ -1,27 +1,34 @@
-//C++program to check whether the given character is vowel or consonant
+/*
 
+Day 2: Program to check whether the given character is vowel or consonant.
+
+*/
+
+// Program
 #include <iostream>
 using namespace std;
-int main() {
+int main()
+{
    char c;
-   cout<<"Enter the character: ";
-   cin>> c;
-   if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c== 'A' || c== 'E' || c == 'I' || c == 'O' || c == 'U'){
-   cout <<c<< " is a Vowel" << endl;
+   cout << "Enter the character: ";
+   cin >> c;
+   if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+   {
+      cout << c << " is a Vowel" << endl;
    }
-   else{
-   cout <<c<< " is a Consonant" << endl;
+   else
+   {
+      cout << c << " is a Consonant" << endl;
    }
    return 0;
 }
 
-
-
-
 /*
-Input:
-   Enter the character: s
 
-Output: 
-   s is a Consonant
+Input:
+Enter the character: s
+
+Output:
+s is a Consonant
+
 */
