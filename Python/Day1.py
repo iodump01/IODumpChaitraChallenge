ada04d3ce4b69a9c43249d6e5f9e07f3e78c7d50
Praneeth Shetty <praneethshetty10@gmail.com>
2023-03-25 22:50:49 +0530

    updating python codes according to the template

--- Python/Day1.py
+++ Python/Day1.py
@@ -1,36 +1,54 @@
-#Day 1: Program to find the largest and smallest of the given 5 numbers
+"""
+Day 1: Program to find the largest and smallest of the given 5 numbers
+"""
 
-a= int(input("Enter First Number: "))
-b= int(input("Enter Second Number: "))
-c= int(input("Enter Third Number: "))
-d= int(input("Enter Fourth Number: "))
-e= int(input("Enter Fifth Number: "))
+# Program
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+d = int(input("Enter Fourth Number: "))
+e = int(input("Enter Fifth Number: "))
 
 
 if a > b and a > c and a > d and a > e:
-  print ('the largest number: ',a)
+    print('The largest number: ', a)
 elif b > a and b > c and b > d and b > e:
-  print ('the largest number: ',b)
+    print('The largest number: ', b)
 elif c > a and c > b and c > d and c > e:
-  print ('the largest number: ',c)
+    print('The largest number: ', c)
 elif d > a and d > b and d > c and d > e:
-  print ('the largest number is: ',d )
+    print('The largest number: ', d)
 elif e > a and e > b and e > c and e > d:
-  print ('the largest number is: ' ,e )
+    print('The largest number: ', e)
 else:
     print("INVALID")
-    
-    
+
+
 if a < b and a < c and a < d and a < e:
-    print ('the smallest number: ',a )
+    print('The smallest number: ', a)
 elif b < a and b < c and b < d and b < e:
-    print ('the smallest number is: ',b )
+    print('The smallest number: ', b)
 elif c < a and c < b and c < d and c < e:
-    print ('the smallest number is: ',c )
+    print('The smallest number: ', c)
 elif d < a and d < b and d < c and d < e:
-    print ('the smallest numbet is: ',d)
+    print('The smallest number: ', d)
 elif e < a and e < b and e < c and e < d:
-    print ('the smallest numbet is: ',e)
+    print('The smallest number: ', e)
 else:
     print("INVALID")
-   
+
+
+"""
+
+Input:
+Enter First Number: 1
+Enter Second Number: 2
+Enter Third Number: 3
+Enter Fourth Number: 4
+Enter Fifth Number: 5
+
+Output:
+The largest number is:  5
+The smallest number:  1
+
+"""
--- Python/Day2.py
+++ Python/Day2.py
@@ -1,18 +1,21 @@
-#Q7 Program to check whether the given character is vowel or consonant
+"""
+Day 2: Program to check whether the given character is vowel or consonant
+"""
 
-x=input("Enter a character: ")
-if (x == 'a' or x== 'A' or x == 'E' or x == 'e' or
-    x == 'i' or x == 'I' or x == 'O' or x == 'o' or x == 'U' or x == 'u'):
+# Program
+x = input("Enter a character: ")
+if (x == 'a' or x == 'A' or x == 'E' or x == 'e' or x == 'i' or x == 'I' or x == 'O' or x == 'o' or x == 'U' or x == 'u'):
     print("Vowel")
 else:
     print("Consonant")
-    
-    
-    
 
 
-"""Input: 
-       Enter a character: A 
-        
-   Output:      
-        Vowel"""
+"""
+
+Input:
+Enter a character: A 
+
+Output:
+Vowel
+
+"""
