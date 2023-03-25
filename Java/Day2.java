9722cf13c58e0f5d78b4bfbf237a1e6143bbbda6
Praneeth Shetty <praneethshetty10@gmail.com>
2023-03-25 23:42:18 +0530

    adding java codes

---
+++ Java/Day2.java
@@ -0,0 +1,36 @@
+/*
+
+Day 2: Program to check whether the given character is vowel or consonant.
+
+*/
+
+// Program
+import java.util.Scanner;
+
+public class Day2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character: ");
+        char c = sc.next().charAt(0);
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+        {
+            System.out.println(c+" is a Vowel");
+        }
+        else
+        {
+            System.out.println(c+" is a Consonant");
+        }
+    }
+}
+
+/*
+
+Input:
+Enter the character:
+s
+
+Output:
+s is a Consonant
+
+
+*/
