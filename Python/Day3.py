37fc797789060d78da40d5a3cbbf816b72bf7ba7
Lavanya Moolya <121078380+Lavanya-227@users.noreply.github.com>
2023-03-26 00:50:42 +0530

    Create Day3.py

---
+++ Python/Day3.py
@@ -0,0 +1,35 @@
+#Q3 Program to find Area of Triangle, Rectangle and Square.
+
+
+b = float(input("Enter base of the Triangle: "))
+h = float(input("Enter height of the Triangle: "))
+area = (b*h)/2
+print("Area of the triangle= ",area)
+print("---------------------------------------------------")
+
+
+w = float(input("Enter width of the rectangle: "))
+l = float(input("Enter length of the rectangle: "))
+area = w * l
+print("Area of the rectangle= ",area)
+print("---------------------------------------------------")
+
+side = float(input("Enter the side of square: "))
+area = side*side
+print("Area of the square= ",area)
+
+
+
+
+"""INPUT and OUTPUT: 
+Enter base of the Triangle: 20.56
+Enter height of the Triangle: 22
+Area of the triangle=  226.16
+---------------------------------------------------
+Enter width of the rectangle: 55
+Enter length of the rectangle: 67
+Area of the rectangle=  3685.0
+---------------------------------------------------
+Enter the side of square: 55
+Area of the square=  3025.0"""
+
