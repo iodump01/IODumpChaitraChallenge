1ba9fa31e8fc47a075fe918e834f85975c2ee99c
Soham Pathak <80677226+sohampathak1234@users.noreply.github.com>
2023-03-30 17:25:53 +0530

    Create Day7.cpp
    Day 8 of our IODump Chaitra challenge!
    
    Program to print multiplication table of a number using for loop

---
+++ CPP/Day7.cpp
@@ -0,0 +1,45 @@
+/* 
+
+Day 7 : Program to print multiplication table of a number using for loop
+
+*/
+
+//Program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    cout << "The Table of " << num << " is " << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+
+    return 0;
+}
+
+
+
+/*
+
+Input and Output
+
+Enter a number : 7
+The Table of 7 is
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+
+*/
