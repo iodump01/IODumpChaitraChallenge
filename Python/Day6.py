1851761dcfff1405534aafc5636fecc5ac613d00
21jiten <72988953+21jiten@users.noreply.github.com>
2023-04-01 01:28:59 +0530

    Create Day6.py

---
+++ Python/Day6.py
@@ -0,0 +1,25 @@
+""" 
+
+Day6: Program to convert given temperature from fahrenheit to celsius
+
+"""
+
+#Program
+fahrenheit =float(input("\nEnter temperature in Fahrenheit: "))
+ 
+celsius = ((fahrenheit-32)*5)/9
+ 
+print('%.2f Fahrenheit is equivalent to %.2f Celsius'%(fahrenheit, celsius))
+
+
+
+
+
+"""
+Input: 
+Enter temperature in Fahrenheit: 56.99
+   
+Output:
+56.99 Fahrenheit is equivalent to 13.88 Celsius
+
+"""
