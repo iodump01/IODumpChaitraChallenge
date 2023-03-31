ce43354e12a9ad324b3a16450b4dfe8b3764a8f0
21jiten <72988953+21jiten@users.noreply.github.com>
2023-04-01 01:30:04 +0530

    Create Day8.py

---
+++ Python/Day8.py
@@ -0,0 +1,29 @@
+"""
+
+Program to print multiplication of tables using for loop
+
+"""
+
+#Program
+n = int(input ("Enter the number: "))    
+for count in range(1, 11):      
+   print (n, 'x', count, '=', n * count)  
+   
+
+
+
+"""
+
+Enter the number: 5
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+
+"""
