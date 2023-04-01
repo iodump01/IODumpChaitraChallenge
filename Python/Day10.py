c71edb4ce5d04e198a0870391fa25b9eaa970feb
21jiten <72988953+21jiten@users.noreply.github.com>
2023-04-02 00:12:36 +0530

    Create Day10.py

---
+++ Python/Day10.py
@@ -0,0 +1,24 @@
+"""
+
+Program to accept the height of a person in cm and convert it to feet. 
+
+"""
+
+#Program
+
+height_cm = float(input("Enter height in centimeters: "))
+
+height_ft = height_cm / 30.48
+
+print("Height in feet: ", height_ft)
+
+
+
+
+"""
+
+Input and Output:
+Enter height in centimeters: 180
+Height in feet:  5.905511811023622 
+    
+"""
