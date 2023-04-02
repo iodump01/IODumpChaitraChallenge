b42ee4fcb93bca1c7e3546ec986c08cb2f55394e
21jiten <72988953+21jiten@users.noreply.github.com>
2023-04-03 00:53:36 +0530

    Create Day11.py

---
+++ Python/Day11.py
@@ -0,0 +1,23 @@
+"""
+Day11: Program to find length of string without using inbuilt function.
+
+"""
+
+# Program
+string = input("Enter a string: ")
+
+length = 0
+for char in string:
+    length += 1
+
+print("Length of the string:", length)
+
+
+"""
+
+Input and Output: 
+Enter a string: Python programming
+Length of the string: 18
+
+
+"""
