6428c281a3c55101d7505206250a45c8180cf48f
Praneeth Shetty <praneethshetty10@gmail.com>
2023-04-12 22:25:52 +0530

    day 17 code

---
+++ Java/Day17.java
@@ -0,0 +1,52 @@
+/*
+
+Day 17:  Program to check whether the given number is palindrome or not.
+
+*/
+
+// Program
+import java.util.Scanner;
+
+public class Day17{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string str: ");
+        String str = sc.nextLine();
+        System.out.println("Enter the substring s: ");
+        String s = sc.nextLine();
+        boolean found = false;
+        
+        int substrLength = s.length();
+        int strLength = str.length();
+
+        if(substrLength <= strLength){
+            for(int i = 0; i < strLength+1-substrLength; i++){
+                String newStr = str.substring(i,i+substrLength);
+                if(s.equals(newStr)){
+                    found = true;
+                    break;
+                }
+            }
+
+            if(found){
+                System.out.println(s + " is a substring of " + str);
+            }else{
+                System.out.println(s + " is not a substring of " + str);
+            }
+        }else{
+            System.out.println(s + " can not be a substring of " + str);
+        }
+    }
+}
+
+/*
+
+Input/Output:
+
+Enter the string str: 
+racecar
+Enter the substring s: 
+rae
+rae is not a substring of racecar
+
+*/
