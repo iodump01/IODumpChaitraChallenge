7ae235e3b73a2ef585809e34dbf622bded8339b1
Praneeth Shetty <praneethshetty10@gmail.com>
2023-04-16 22:52:08 +0530

    adding code for day 22 - 15

---
+++ Java/Day24.java
@@ -0,0 +1,53 @@
+/*
+
+Day 24: Write a program that takes a list of integers as input and returns a new list that contains only the elements that appear more than once in the original list.
+*/
+
+// Program
+import java.util.*;
+
+public class Day24{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        System.out.println("Enter the elements (space seperated): ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> newarr = new ArrayList<Integer>();
+        
+        for(int i = 0; i < n; i++){
+            boolean haveMore = false;
+            for(int j = 0; j < n; j++){
+                if(i != j && arr[i] == arr[j]){
+                    haveMore = true;
+                    break;
+                }
+            }
+            if(haveMore){
+                if(newarr.indexOf(arr[i]) == -1){
+                    newarr.add(arr[i]);
+                }
+            }
+        }
+
+        System.out.print("Output Array: " + newarr);
+
+    }
+}
+
+/*
+
+Input/Output:
+
+Enter the number of elements: 
+10
+Enter the elements (space seperated): 
+1 2 3 4 2 3 4 2 4 2 
+Output Array: [2, 3, 4]
+
+*/
---
+++ Java/Day25.java
@@ -0,0 +1,43 @@
+/*
+
+Day 25: Print the following pattern:
+         *****
+          ***
+           *
+*/
+
+// Program
+import java.util.Scanner;
+
+public class Day25{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number n: ");
+        int n = sc.nextInt();
+
+        for(int i = n-1; i >= 0; i--){
+            for(int k = 1; k <= n-1-i; k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= (2*i+1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+/*
+
+Input/Output:
+
+Enter the number n: 
+5
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
