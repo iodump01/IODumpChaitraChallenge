081c8e5a1186d8bdc6c4e755008b271d0991b61f
Lavanya Moolya <121078380+Lavanya-227@users.noreply.github.com>
2023-04-26 21:29:42 +0530

    Create Day30.cpp

---
+++ CPP/Day30.cpp
@@ -0,0 +1,61 @@
+/*
+
+Day 30:	Implement Binary Search Algorithm.
+
+*/
+
+
+//Program
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, arr[10], num, first, last, middle;
+    cout<<"Enter 10 Elements (in ascending order): \n";
+    for(i=0; i<10; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to be Search: ";
+    cin>>num;
+    first = 0;
+    last = 9;
+    middle = (first+last)/2;
+    while(first <= last)
+    {
+        if(arr[middle]<num)
+            first = middle+1;
+        else if(arr[middle]==num)
+        {
+            cout<<"\nThe number "<<num<<" found at Position "<<middle+1;
+            break;
+        }
+        else
+            last = middle-1;
+        middle = (first+last)/2;
+    }
+    if(first>last)
+        cout<<"\nThe number, "<<num<<" is not found in given Array";
+    cout<<endl;
+    return 0;
+}
+
+
+/*
+
+Input and Output:
+Enter 10 Elements (in ascending order): 
+12
+13
+15
+17
+19
+20
+30
+40
+50
+66
+
+Enter Element to be Search: 19
+
+The number 19 found at Position 5
+
+*/
